b6870f1a08d1ba1e3ad8a221e9d393bf85ad8b4f
OluC94 <coker.olu94@gmail.com>
2024-06-27 13:24:20 +0100

    pass all tests

--- src/toggle.js
+++ src/toggle.js
@@ -1,3 +1,11 @@
-const toggle = (criteria, array) => {}
+const toggle = (targetId, array) => {
+    const results = array.map(item => {
+        if (item.id === targetId){
+            return {...item, isOn: !item.isOn}
+        }
+        return item
+    })
+    return results
+}
 
 export {toggle}
